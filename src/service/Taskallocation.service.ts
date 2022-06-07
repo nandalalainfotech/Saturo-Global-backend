389b90389618c3360ae666ce1658944c1b542623
Nandalalainfotech <nandalalainfotech.com>
2022-06-07 18:42:04 +0530

    Backend taskAllocation changes are updated by Moorthy

--- src/service/Taskallocation.service.ts
+++ src/service/Taskallocation.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import multer from "multer";
-
 import { TaskallocationDTO } from "src/dto/Taskallocation001wb.dto";
 import { Taskallocation001wb } from "src/entity/Taskallocation001wb";
+import { User001mb } from "src/entity/User001mb";
 import { Repository } from "typeorm";
+
 var fs = require('fs');
 const Excel = require('exceljs');
 const reader = require('xlsx');
@@ -13,11 +13,14 @@ const reader = require('xlsx');
 export class TaskallocationService {
 
     constructor(
+        @InjectRepository(User001mb) private readonly userRepository: Repository<User001mb>,
         @InjectRepository(Taskallocation001wb) private readonly taskAllocateRepository: Repository<Taskallocation001wb>) {
 
     }
     async create(file: any, taskallocationDTO: TaskallocationDTO): Promise<Taskallocation001wb[]> {
         // taskallocation001wb.setProperties(taskallocationDTO);
+        // console.log("file----------->>>>", file);
+
         this.taskAllocateRepository.clear();
 
         fs.writeFile('helloworld.xlsx', file.buffer, function (err) {
@@ -27,10 +30,13 @@ export class TaskallocationService {
 
         const file2 = reader.readFile("helloworld.xlsx")
         const sheet1 = reader.utils.sheet_to_json(file2.Sheets[file2.SheetNames[0]]);
+
         let sheet = JSON.parse(JSON.stringify(sheet1).replace(/\s(?=\w+":)/g, ""));
-        // console.log("sheet----------->>>>", sheet);
 
         let taskallocation001wbs: Taskallocation001wb[] = [];
+        let reviewers: User001mb[] = [];
+        reviewers = await this.userRepository.find({ relations: ["person","role"], where: { roleid: 3 }});
+        // console.log("UserList",reviewer);
         for (let i = 0; i < sheet.length; i++) {
             const taskallocation001wb = new Taskallocation001wb();
             taskallocation001wb.curatorId = i + 1;
@@ -38,22 +44,26 @@ export class TaskallocationService {
             taskallocation001wb.cbatchNo = "B1";
             taskallocation001wb.curatorTanNo = sheet[i].TANNUMBER;
             taskallocation001wb.curatorAllocateDate = new Date();
-            taskallocation001wb.reviewerName = sheet[i].REVIEWERNAME
-            taskallocation001wb.rbatchNo = "B1";
-            taskallocation001wb.reviewerTanNo = sheet[i].TANNUMBER_1;
-            taskallocation001wb.filename = "task allocation";
-            taskallocation001wb.reviewerAllocateDate = new Date();
             taskallocation001wb.insertUser = taskallocationDTO.insertUser;
             taskallocation001wb.insertDatetime = taskallocationDTO.insertDatetime;
+            
 
+            let random = Math.floor(Math.random() * reviewers.length);
+            taskallocation001wb.reviewerName = reviewers[random].username;
+            taskallocation001wb.reviewerTanNo = sheet[i].TANNUMBER;
             this.taskAllocateRepository.save(taskallocation001wb);
             taskallocation001wbs.push(taskallocation001wb);
-            // console.log("taskallocation001wb", taskallocation001wb);
+            console.log("taskallocation001wb for reviewer-----------------", taskallocation001wb);
         }
-
+        
         return taskallocation001wbs;
 
     }
+
+   
+        
+  
+
     async update(taskallocationDTO: TaskallocationDTO): Promise<Taskallocation001wb> {
         const taskallocation001wb = new Taskallocation001wb();
         taskallocation001wb.setProperties(taskallocationDTO);
